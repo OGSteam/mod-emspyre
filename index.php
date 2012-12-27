fdbf5558c35ca1f68f953af84d015ace96fbdf25
erell <erell@erell-PC>
2012-12-27 19:01:56 +0100

    pause ....

--- index.php
+++ index.php
@@ -10,6 +10,52 @@ global $db, $table_preffix;
 
 require_once("views/page_header.php");
 
+$user_empire = user_get_empire();
+
+$user_building = $user_empire["building"];
+$user_defence = $user_empire["defence"];
+$user_technology = $user_empire["technology"];
+
+
+$nb_plapla =  find_nb_planete_user();
+
+// on va réordonner tout ca
+$account = array();
+$j=0;
+$i=0;
+
+//$account = inverse_array_empire($user_building,$nb_plapla,100);
+//$account = inverse_array_empire($user_defence,$nb_plapla,100);
+//$account = array_merge($account, inverse_array_empire($user_defence,$nb_plapla,100) ); // ajout defense
+//$account = array_merge($account, inverse_array_empire($user_technology,$nb_plapla,100) ); // ajout techno
+
+//$futur_tab= add_first_colonne($user_technology);
+/// faut faire tableau avec
+/// premiere ligne les noms
+/// premiere colonne les noms
+
+//foreach($user_empire["building"] as $cc => $ccc)
+
+
+
+var_dump($futur_tab);
+
+
+
+//affichage
+foreach($futur_tab as $count => $count_bis)
+{
+    echo '[tr]';
+   foreach( $count_bis as $c)
+    {
+        echo '[td]';
+         echo $c;
+          echo '[/td]';
+    }
+    //echo $count ;
+    echo '[/tr]';
+    
+}
 
 
 require_once("views/page_tail.php"); //on inclus la queue d'ogspy
@@ -18,5 +64,34 @@ require_once("views/page_tail.php"); //on inclus la queue d'ogspy
 //Voila le mod est fini ! Pas trop triste ? :D
 
 
+
+
+
+function inverse_array_empire($array , $nb_plapla , $step_planete)
+{
+    $account = array();
+    for ($i=101 ; $i<=$nb_plapla+100 ; $i++) 
+{
+    foreach($array[$i] as $clef => $value)
+    {   
+        $account[$clef][] = $value;
+    }
+     
+   // echo '<br />';
+   
+ }
+ return  $account;
+}
+
+function add_first_colonne($account)
+{
+    $futur_tab= array();
+    foreach($account as $count => $count_bis)
+{
+   array_unshift($count_bis,$count) ; // on ajoute la premiere colonne
+   $futur_tab[] = $count_bis ;
+   }
+return $futur_tab;
+}
 ?>
 
